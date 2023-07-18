de68834c1f3b0de064eec3373a2a2a05c114a849
marcozegarra <marcozegarraaquima@gmail.com>
2023-07-18 18:28:00 -0500

    mirepositorio

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import C01componente from "./componentes/C01componente";
+import C02contador from "./componentes/C02contador";
+import C03doblecontador from "./componentes/C03doblecontador";
+
+
+function App() {
+  return (
+    <div>
+      <C01componente/>
+
+      <hr/>
+      <C02contador/>
+
+      <hr/>
+      <C03doblecontador/>
+
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/C01componente.jsx
@@ -0,0 +1,12 @@
+import React from 'react'       //rafce
+
+const C01componente = () => {
+  return (
+    
+    <div>
+      Hola mundo desde React;
+    </div>
+  )
+}
+
+export default C01componente
